9b1ec1b72bedb146c12efd1220feaf0980e32ddf
BitCoding <bitcoding@bitcoding.eu>
2014-07-29 21:47:05 +0200

    Little Changes

--- BitBase.php
+++ BitBase.php
@@ -660,7 +660,7 @@ class BitBase {
     }
 
     public static function getIP() {
-        return isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER["REMOTE_ADDR"];
+        return PROXY_MODE && $ip = Vars::server_getIP('HTTP_X_FORWARDED_FOR') ? $ip : $_SERVER["REMOTE_ADDR"];
     }
 
 }
