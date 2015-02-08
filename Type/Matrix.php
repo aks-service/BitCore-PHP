2a5b08a910d5509d84257891d723b9d983987056
Bitcoding <bitcoding@bitcoding.eu>
2015-02-08 15:22:51 +0100

    matrix

--- Type/Matrix.php
+++ Type/Matrix.php
@@ -8,7 +8,7 @@ class Matrix{
         foreach ($array1 as $key => $value) {
             if (isset($array2[$key])) {
                 if ($depth && is_array($array2[$key])) {
-                    $merged [$key] = self::MergeRecursiveDistinct($array2[$key], $value, $depth--);
+                    $merged [$key] = self::MergeRecursiveDistinct($value, $array2[$key], $depth - 1);
                 } else {
                     $merged [$key] = $array2[$key];
                 }
